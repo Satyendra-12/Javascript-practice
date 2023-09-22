14ce39b96c35bd9f57424e34068bb3df5a9fe499
Satyendra Kumar <satyendrakumar8672461@gmail.com>
2023-09-23 04:51:14 +0530

    string methods

---
+++ string.js
@@ -0,0 +1,16 @@
+let s = "Satyendra"
+console.log(s.toUpperCase())
+console.log(s.toLowerCase())
+console.log(s.charAt(5))
+console.log(s.indexOf('d'))
+let p ="kumar"
+console.log(s.concat(" ",p))
+console.log(s.substring(0,4))
+console.log(s.slice(-1,3))
+
+let b = "  Pilibhit  "
+console.log(b)
+console.log(b.trim())
+
+let k= "orange grapes banana tomato"
+console.log(k.split(" "))
